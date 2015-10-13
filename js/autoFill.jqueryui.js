8dc0ec4b2c8386a8195535de94f0106420a87f55
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-10-13 13:33:56 +0100

    Updated: AutoFill now displays the drag handle using the cell's offset parent to position the element rather than using `position:relative` on all table cells. This improves rendering in all browsers and performance in IE.
    - Thread 30198
    - Also rendering error with cell border showing in the KeyTable focus
      when used with KeyTable due to the position relative, reported by Dan
      R
    
    Sync to source repo @3c31000aa40688cdcfbbcd8bbfbfeaa047f760e1

--- js/autoFill.jqueryui.js
+++ js/autoFill.jqueryui.js
@@ -1,2 +1,33 @@
+/*! jQuery UI integration for DataTables' AutoFill
+ * ©2015 SpryMedia Ltd - datatables.net/license
+ */
 
-$.fn.dataTable.AutoFill.classes.btn = 'ui-button ui-state-default ui-corner-all';
+(function( factory ){
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery', 'datatables.net-jqui', 'datatables.net-autofill'], factory );
+	}
+	else if ( typeof exports === 'object' ) {
+		// Node / CommonJS
+		module.exports = function ($) {
+			if ( ! $ ) { $ = require('jquery'); }
+			if ( ! $.fn.dataTable ) { require('datatables.net-jqui')($); }
+			if ( ! $.fn.dataTable.AutoFill ) { require('datatables.net-autofill')($); }
+
+			factory( $ );
+		};
+	}
+	else {
+		// Browser
+		factory( jQuery );
+	}
+}(function( $ ) {
+'use strict';
+var DataTable = $.fn.dataTable;
+
+
+DataTable.AutoFill.classes.btn = 'ui-button ui-state-default ui-corner-all';
+
+
+return DataTable;
+}));
