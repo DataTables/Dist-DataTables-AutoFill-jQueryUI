b4a3ee1ebd1e95784fa5fdb2e2cadc98f2d4e1e2
Allan Jardine <allan@datatables.net>
2022-09-25 15:46:56 +0000

    Sync DistRepos

--- js/autoFill.jqueryui.js
+++ js/autoFill.jqueryui.js
@@ -1,3 +1,4 @@
+
 /*! jQuery UI integration for DataTables' AutoFill
  * ©2015 SpryMedia Ltd - datatables.net/license
  */
@@ -5,7 +6,7 @@
 (function( factory ){
 	if ( typeof define === 'function' && define.amd ) {
 		// AMD
-		define( ['jquery', 'datatables.net-jqui', 'datatables.net-autofill'], function ( $ ) {
+		define( ['jquery', 'datatables.net-ju', 'datatables.net-autofill'], function ( $ ) {
 			return factory( $, window, document );
 		} );
 	}
@@ -13,16 +14,25 @@
 		// CommonJS
 		module.exports = function (root, $) {
 			if ( ! root ) {
+				// CommonJS environments without a window global must pass a
+				// root. This will give an error otherwise
 				root = window;
 			}
 
-			if ( ! $ || ! $.fn.dataTable ) {
-				$ = require('datatables.net-jqui')(root, $).$;
+			if ( ! $ ) {
+				$ = typeof window !== 'undefined' ? // jQuery's factory checks for a global window
+					require('jquery') :
+					require('jquery')( root );
+			}
+
+			if ( ! $.fn.dataTable ) {
+				require('datatables.net-ju')(root, $);
 			}
 
 			if ( ! $.fn.dataTable.AutoFill ) {
 				require('datatables.net-autofill')(root, $);
 			}
+
 
 			return factory( $, root, root.document );
 		};
@@ -34,6 +44,7 @@
 }(function( $, window, document, undefined ) {
 'use strict';
 var DataTable = $.fn.dataTable;
+
 
 
 DataTable.AutoFill.classes.btn = 'ui-button ui-state-default ui-corner-all';
