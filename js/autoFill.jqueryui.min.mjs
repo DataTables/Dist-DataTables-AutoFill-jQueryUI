929a873cffcfcac8254fd978f29bde4ce21f2ae2
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 17:05:45 +0000

    d6b7ad76c39d470889acb35b9709af9865002f59
    Sync to source repo @d6b7ad76c39d470889acb35b9709af9865002f59

--- js/autoFill.jqueryui.min.mjs
+++ js/autoFill.jqueryui.min.mjs
@@ -1,1 +1,4 @@
-import DataTable from"";DataTable.AutoFill.classes.btn="ui-button ui-state-default ui-corner-all";export default DataTable;
+/*! jQuery UI integration for DataTables' AutoFill
+ * ©2015 SpryMedia Ltd - datatables.net/license
+ */
+import $ from"jquery";import DataTable from"datatables.net-ju";import AutoFill from"datatables.net-autofill";DataTable.AutoFill.classes.btn="ui-button ui-state-default ui-corner-all";export default DataTable;
