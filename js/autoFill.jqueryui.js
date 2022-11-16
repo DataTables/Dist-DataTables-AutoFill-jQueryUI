ef5de66120c4f7cb87eb86357602ee8756600a08
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-16 17:02:31 +0000

    49a19d6fc4b402d895a464737c8c580fb2bb0fc8
    Sync to source repo @49a19d6fc4b402d895a464737c8c580fb2bb0fc

--- js/autoFill.jqueryui.js
+++ js/autoFill.jqueryui.js
@@ -5,7 +5,7 @@
 (function( factory ){
 	if ( typeof define === 'function' && define.amd ) {
 		// AMD
-		define( ['jquery', 'datatables.net-ju', 'datatables.net-autofill'], function ( $ ) {
+		define( ['jquery', 'datatables.net-jqui', 'datatables.net-autofill'], function ( $ ) {
 			return factory( $, window, document );
 		} );
 	}
@@ -25,13 +25,12 @@
 			}
 
 			if ( ! $.fn.dataTable ) {
-				require('datatables.net-ju')(root, $);
+				require('datatables.net-jqui')(root, $);
 			}
 
 			if ( ! $.fn.dataTable.AutoFill ) {
 				require('datatables.net-autofill')(root, $);
 			}
-
 
 			return factory( $, root, root.document );
 		};
--- js/autoFill.jqueryui.min.mjs
+++ js/autoFill.jqueryui.min.mjs
@@ -1,4 +1,4 @@
 /*! jQuery UI integration for DataTables' AutoFill
  * ©2015 SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-ju";import AutoFill from"datatables.net-autofill";DataTable.AutoFill.classes.btn="ui-button ui-state-default ui-corner-all";export default DataTable;
+import $ from"jquery";import DataTable from"datatables.net-jqui";import AutoFill from"datatables.net-autofill";DataTable.AutoFill.classes.btn="ui-button ui-state-default ui-corner-all";export default DataTable;
