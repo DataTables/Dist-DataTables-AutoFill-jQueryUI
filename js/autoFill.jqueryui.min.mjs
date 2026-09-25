23212486490d57af3e34203b9ac8b34bd1c90d30
dtbuild <dtbuild@rigil>
2026-09-25 10:56:26 +0000

    a4d18c4a57d827d8aa91c3e61aa10dc3453a856b Release 3.1.2
    Sync to source repo @a4d18c4a57d827d8aa91c3e61aa10dc3453a856b

--- js/autoFill.jqueryui.min.mjs
+++ js/autoFill.jqueryui.min.mjs
@@ -1,4 +1,4 @@
-/*! AutoFill jQuery UI styling 3.1.1 for DataTables
+/*! AutoFill jQuery UI styling 3.1.2 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-jqui";import AutoFill from"datatables.net-autofill";DataTable.AutoFill.classes.btn="ui-button ui-state-default ui-corner-all";export default DataTable;
--- js/autoFill.jqueryui.mjs
+++ js/autoFill.jqueryui.mjs
@@ -1,4 +1,4 @@
-/*! AutoFill jQuery UI styling 3.1.1 for DataTables
+/*! AutoFill jQuery UI styling 3.1.2 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
